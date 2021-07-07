c14905970a383dd4c52896acaa99e74e2fed0375
VladimirZaitsev21 <vlzaitsev2000@gmail.com>
2021-07-07 19:41:58 +0300

    Refactoring main method.

--- src/main/java/ru/zvo/application/App.java
+++ src/main/java/ru/zvo/application/App.java
@@ -9,22 +9,21 @@ import java.util.stream.Collectors;
 public class App {
 
     public static final int MONEY_RATIO = 10000;
+
     public static void main(String[] args) {
-        List<Client> clients = new ArrayList<>();
-        List<RequestProcessor> processors = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
-            clients.add(new Client("Client #" + (i + 1), getRandomMoney(), defineOperationType(i)));
-        }
+        //Creating clients
+        List<Client> clients = createClients(5);
 
-        for (int i = 0; i < 2; i++) {
-            processors.add(new RequestProcessor("Request processor #" + (i + 1)));
-        }
+        //Creating request processors
+        List<RequestProcessor> processors = createProcessors(2);
 
+        //Creating clients threads
         List<Thread> clientThreads = new ArrayList<>();
         clientThreads.addAll(clients.stream().map(client -> new Thread(client, client.getName())).collect(Collectors.toList()));
         clientThreads.forEach(Thread::start);
 
+        //Creating processors threads
         List<Thread> processorThreads = new ArrayList<>();
         processorThreads.addAll(processors.stream().map(processor -> new Thread(processor, processor.getName())).collect(Collectors.toList()));
         processorThreads.forEach(Thread::start);
@@ -41,6 +40,22 @@ public class App {
 
     }
 
+    private static List<Client> createClients(int clientsAmount) {
+        List<Client> clients = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            clients.add(new Client("Client #" + (i + 1), getRandomMoney(), defineOperationType(i)));
+        }
+        return clients;
+    }
+
+    private static List<RequestProcessor> createProcessors(int processorsAmount) {
+        List<RequestProcessor> processors = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            processors.add(new RequestProcessor("Request processor #" + (i + 1)));
+        }
+        return processors;
+    }
+
     private static BankOperationType defineOperationType(int i) {
         if (i % 2 == 0) {
             return BankOperationType.CREDIT;
@@ -50,6 +65,6 @@ public class App {
     }
 
     private static int getRandomMoney() {
-        return  (int) (MONEY_RATIO * Math.random());
+        return (int) (MONEY_RATIO * Math.random());
     }
 }
